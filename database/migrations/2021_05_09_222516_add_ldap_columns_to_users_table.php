3b72d43dc0fbd5d968ec62a6b9af65befa83c55d
root <root@192.168.1.2>
2021-05-22 20:24:54 -0300

    ahora conecta con bd externo

--- database/migrations/2021_05_09_222516_add_ldap_columns_to_users_table.php
+++ database/migrations/2021_05_09_222516_add_ldap_columns_to_users_table.php
@@ -12,7 +12,7 @@ class AddLdapColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('guid')->unique()->nullable();
+            $table->string('guid')->nullable();
             $table->string('domain')->nullable();
         });
     }
