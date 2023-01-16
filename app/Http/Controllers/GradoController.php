71b15056053f702aa3fd84405e782eaf5c57c8f5
Agustin Kuster <61338886+aguskuster@users.noreply.github.com>
2023-01-16 20:13:27 -0300

    Agregar grupo grado

--- app/Http/Controllers/GradoController.php
+++ app/Http/Controllers/GradoController.php
@@ -11,16 +11,29 @@ class GradoController extends Controller
         $request->validate([
             'grado'=> 'required|string',
             'materias'=> 'array',
+            'grupos'=> 'array',
         ]);
         $grado=Grado::findOrFail($id);
         $grado->update($request->all());
         if($request->materias){
             $grado->materias()->sync($request->materias);
         }
+        if($request->grupos){
+            $this->agregarGruposGrado($request->grupos, $grado);
+        }
 
         RegistrosController::store("GRADO", $request->header('token'), "UPDATE", $grado->carrera->nombre . " Grado: " . $grado->grado);
 
         return response()->json($grado->load('materias'));
-        
+
+    }
+
+    public function agregarGruposGrado($grupos,$grado){
+        foreach ($grupos as $grupo) {
+            if($grado->grupos()->where('idGrupo', $grupo->idGrupo)->first()){
+                continue;
+            }
+            $grado->grupos()->create($grupo);
+        }
     }
 }
