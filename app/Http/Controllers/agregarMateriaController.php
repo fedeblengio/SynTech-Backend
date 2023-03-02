30f170ad1bedaa1cb8976d614fab234c22773863
AforGaming <aaronnoguera07@gmail.com>
2023-03-02 18:59:18 -0300

    prueba de vuelta

--- app/Http/Controllers/agregarMateriaController.php
+++ app/Http/Controllers/agregarMateriaController.php
@@ -87,7 +87,6 @@ class agregarMateriaController extends Controller
         $materia->save();
         RegistrosController::store("MATERIA", $request->header('token'), "CREATE", $request->nombre);
         return $materia;
-        
     }
 
 
