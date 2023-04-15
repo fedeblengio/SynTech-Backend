45189f3241feb412b21c7e5418bdbb72b3efecc3
Agustin Kuster <akusterpiriz@gmail.com>
2023-04-15 18:52:37 -0300

    delete test db

--- config/database.php
+++ config/database.php
@@ -63,24 +63,7 @@ return [
             ]) : [],
         ],
 
-        'testing' => [
-            'driver' => 'mysql',
-            'host' => env('TEST_DB_HOST', '127.0.0.1'),
-            'port' => env('TEST_DB_PORT', '3306'),
-            'database' => env('TEST_DB_DATABASE', 'test_database'),
-            'username' => env('TEST_DB_USERNAME', 'test_user'),
-            'password' => env('TEST_DB_PASSWORD', 'test_password'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+     
 
         'pgsql' => [
             'driver' => 'pgsql',
