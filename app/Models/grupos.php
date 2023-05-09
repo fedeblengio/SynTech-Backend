2dd6426c5f00760778af9789e75016ed82f18baf
Agustin Kuster <akusterpiriz@gmail.com>
2023-05-08 23:19:19 -0300

    Eliminar Carreras completo

--- app/Models/grupos.php
+++ app/Models/grupos.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class grupos extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     protected $table = 'grupos';
     protected $fillable = ['idGrupo', 'nombreCompleto','anioElectivo','grado_id'];
