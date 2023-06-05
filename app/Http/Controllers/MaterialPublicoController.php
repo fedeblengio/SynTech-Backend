4216d24d54b0a9c4f2a1877c1426a68891964c30
Agustin Kuster <akusterpiriz@gmail.com>
2023-06-05 19:07:43 -0300

    fast fix

--- app/Http/Controllers/MaterialPublicoController.php
+++ app/Http/Controllers/MaterialPublicoController.php
@@ -36,10 +36,11 @@ class MaterialPublicoController extends Controller
 
     public function index(Request $request)
     {
-        $peticionSQL = DB::table('bedelias')
+
+
+            $peticionSQL = DB::table('material_publicos')
             ->select('material_publicos.id', 'material_publicos.imgEncabezado', 'material_publicos.titulo AS titulo', 'material_publicos.mensaje AS mensaje', 'material_publicos.idUsuario', 'material_publicos.imgEncabezado', 'material_publicos.created_at AS fecha', 'usuarios.nombre AS nombreAutor')
-            ->join('usuarios', 'usuarios.id', '=', 'bedelias.id')
-            ->join('material_publicos', 'material_publicos.idUsuario', '=', 'bedelias.id')
+            ->join('usuarios', 'usuarios.id', '=', 'material_publicos.idUsuario')
             ->orderBy('material_publicos.id', 'desc')
             ->take($request->limit)
             ->get();
